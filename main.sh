7a5f494149ed126f4eb53102aa936fd59830a896
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-11-17 13:11:14 -0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,31 @@
+RED='\033[0;31m'
+echo 'sudo TotallyRealPackageManager-get update && sudo TotallyRealPackageManager upgrade'
+echo [===============]
+sleep 6.14
+clear
+echo [#==============]
+sleep 4.85
+clear
+echo [###============]
+sleep 0.43
+clear
+echo [####===========]
+sleep 8.54
+clear
+echo [######=========]
+sleep 2.89
+clear
+echo [#######========]
+sleep 1.74
+clear
+echo [###########====]
+sleep 7.53
+clear
+echo [############===]
+sleep 0.76
+clear
+echo [##############=]
+sleep 42.97
+clear
+echo -e "##############${RED}E${RED}"
+echo -e "${RED}ERROR 69: No loading for you today!${RED}"
